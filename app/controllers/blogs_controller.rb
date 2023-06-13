604ccf4f541b828ceea91f24b5642616a6fdf487
Sarrick <57643991+ShifHub@users.noreply.github.com>
2023-06-12 21:17:55 -0400

    Implemented styles for the initial blog layout

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -77,4 +77,5 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
     end
+    
 end
--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,36 @@
 module BlogsHelper
+
+    def blog_debut(num)
+        blog = @blogs[num]
+        if blog
+            first_sentence(blog) + '..'
+        else
+            'Coming soon..'
+        end
+    end
+
+    def blog_title(num)
+        blog = @blogs[num]
+        if blog
+            blog.title
+        else
+            'New Posts'
+        end
+    end
+
+    def blog_date(num)
+        blog = @blogs[num]
+        if blog
+            blog.created_at.strftime('%m/%d/%Y')
+        else
+            '--/--/--'
+        end
+    end
+
+    private
+    ##helpers
+    def first_sentence(blog)
+        sentences = blog.body.split('.')
+        sentences[0] + '.'
+    end
 end

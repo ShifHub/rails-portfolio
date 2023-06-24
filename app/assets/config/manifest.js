d08105fe5c7fa72c1ef4716fefa2ee01db5760af
Sarrick <57643991+ShifHub@users.noreply.github.com>
2023-06-24 17:11:14 -0400

    Integrated video background along with styles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,9 @@
 //= link_tree ../images
+//= link_tree ../videos
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
 //= require rails-ujs
 //= require jquery3
 //= require popper

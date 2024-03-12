eb60ff89245de63b8c95c419519cc3890b46fac9
Sarrick <57643991+ShifHub@users.noreply.github.com>
2024-03-12 00:14:03 -0700

    Utilized different naming conventions for order tracking of portfolio items.

--- db/migrate/20230619001049_add_row_order_to_portfolios.rb
+++
@@ -1,5 +0,0 @@
-class AddRowOrderToPortfolios < ActiveRecord::Migration[7.0]
-  def change
-    add_column :portfolios, :row_order, :integer, null: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_001049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
-    t.integer "row_order"
   end
 
   create_table "skills", force: :cascade do |t|

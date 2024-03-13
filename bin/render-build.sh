f117d1b436da594bc0a4d9fa87e0ee8a07b5675f
Sarrick <57643991+ShifHub@users.noreply.github.com>
2024-03-12 22:39:44 -0700

    Added admin designation to the build script for site control

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,3 +8,9 @@ bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+rails runner "user = User.find_or_initialize_by(email: ENV['ADMIN_EMAIL'])
+              user.password = ENV['ADMIN_PASSWORD']
+              user.password_confirmation = ENV['ADMIN_PASSWORD']
+              user.roles = 'site_admin'
+              user.save!"

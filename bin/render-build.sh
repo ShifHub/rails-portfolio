e020f0bded9d5682ca6f324a4a39f0eb895fb78c
Sarrick <57643991+ShifHub@users.noreply.github.com>
2024-03-13 02:00:05 -0700

    Introduced error checking for admin designation

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,8 +9,16 @@ bundle exec rails db:seed
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-rails runner "user = User.find_or_initialize_by(email: ENV['ADMIN_EMAIL'])
-              user.password = ENV['ADMIN_PASSWORD']
-              user.password_confirmation = ENV['ADMIN_PASSWORD']
-              user.roles = 'site_admin'
-              user.save!"
+rails runner "if ENV['ADMIN_EMAIL'].present?
+                user = User.find_or_initialize_by(email: ENV['ADMIN_EMAIL'])
+                user.password = ENV['ADMIN_PASSWORD']
+                user.password_confirmation = ENV['ADMIN_PASSWORD']
+                user.roles = 'site_admin'
+                if user.save
+                    puts \"User: \#{user.email} is successfully set up as admin\"
+                else
+                    puts \"Failed to save user: \#{user.errors.full_messages.join(', ')}\"
+                end
+              else
+                puts 'ADMIN_EMAIL environment variables must be set.'
+              end"
